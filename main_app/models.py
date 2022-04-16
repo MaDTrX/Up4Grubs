90a0c84855bf992d373586015da20dbe31de60ee
MARLIN K MAKORI <marlin.makori@gmail.com>
2022-04-15 18:13:00 -0700

    started auth
    fixed migrations

--- main_app/models.py
+++ main_app/models.py
@@ -1,5 +1,9 @@
 from email.policy import default
 from django.db import models
+from datetime import date
+import datetime
+#import timedelta
+from django.contrib.auth.models import User
 
 TYPE = (
     ('F','Fresh Produce'),
@@ -7,19 +11,23 @@ TYPE = (
 )
 # Create your models here.
 class Grub(models.Model):
-    type: models.CharField(
+    item =  models.CharField(max_length=50)
+    type = models.CharField(
         max_length = 1,
         choices = TYPE,
     )
-    exp: models.DateField(
-        'exp date'
-        # default=
+
+    exp = models.DateField(
+        'exp date', 
+         #! + timedelta(days=7)
         #* set exp date to 3 days after the created date
         )
     #TODO set default date
     # photo: models.BinaryField
-    desc: models.TextField(max_length = 250)
-    price: models.IntegerField()
+    desc =  models.TextField(max_length=250)
+    price = models.IntegerField()
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
        return self.type
--- main_app/views.py
+++ main_app/views.py
@@ -8,11 +8,11 @@ from django.http import HttpResponse
 
 # Define the home view
 def home(request):
-    grubs = Grub.objects.all()
-    #* gets all grubs| class Grub is imported from models
-
-    return render(request, 'grubs/index.html', {'grubs': grubs})
-    #! define  a function called grubs
+    # grubs = Grub.objects.all()
+    # console.log(grubs)
+#     #* gets all grubs| class Grub is imported from models
+    return render(request, 'home.html')
+#     #! define  a function called grubs
 
 def about(request):
     return render(request, 'about.html')
