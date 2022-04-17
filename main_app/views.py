dd34e529dec937f662c4db96969b1a539d46d8d5
vergmanu <vergmanu@gmail.com>
2022-04-16 17:59:56 -0700

    implements authentication

--- main_app/views.py
+++ main_app/views.py
@@ -1,8 +1,11 @@
-from dataclasses import field
 from django.shortcuts import render
-from .models import Grub 
 from django.views.generic import ListView, DetailView
 from django.views.generic.edit import UpdateView, CreateView, DeleteView
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+from .models import Grub 
+from dataclasses import field
 
 # Create your views here.
 from django.http import HttpResponse
@@ -20,25 +23,43 @@ def home(request):
 def about(request):
     return render(request, 'about.html')
 
+def signup(request):
+  error_message = ''
+  if request.method == 'POST':
+    form = UserCreationForm(request.POST)
+    if form.is_valid():
+      user = form.save()
+      login(request, user)
+      return redirect('index')
+    else:
+      error_message = 'Invalid sign up - try again'
+  form = UserCreationForm()
+  context = {'form': form, 'error_message': error_message}
+  return render(request, 'registration/signup.html', context)
+
 class GrubList(ListView):
     model = Grub
     
-class GrubCreate(CreateView):
+class GrubDetail(LoginRequiredMixin, DetailView):
+    model = Grub
+
+class GrubCreate(LoginRequiredMixin, CreateView):
     model = Grub
     fields = '__all__'
     success_url = '/grubs/'
+    def form_valid(self, form):
+    # Assign the logged in user (self.request.user)
+        form.instance.user = self.request.user  # form.instance is the grub
+    # Let the CreateView do its job as usual
+        return super().form_valid(form)
 
-class GrubDelete(DeleteView):
-    model = Grub
-    success_url = '/grubs/'
-
-class GrubDetail(DetailView):
-    model = Grub
-
-class GrubUpdate(UpdateView):
+class GrubUpdate(LoginRequiredMixin, UpdateView):
     model = Grub 
     fields = '__all__'
     success_url = '/grubs/'
     # find a way to attach the id at the end of success url
 
+class GrubDelete(LoginRequiredMixin, DeleteView):
+    model = Grub
+    success_url = '/grubs/'
 
